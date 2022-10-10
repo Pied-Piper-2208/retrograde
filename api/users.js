f15616b4387839bb4547b633908c30321cf0aa96
aldwinverzosa <aldwinverzosa@gmail.com>
2022-10-10 08:56:25 -0500

    added register backend and front end

---
+++ api/users.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const { getUserByUsername, createUser, getUserById} = require('../db');
+
+
+
+router.use((req, res, next) => {
+    console.log("A request is being made to /users");
+  
+    next();
+    
+  });
+  
+  // POST /api/users/login
+router.post('/login', async (req, res, next) => {
+
+    const { username, password } = req.body;
+    console.log("username/password is ", username, password);
+    //console.log("req is ", req);
+    
+    // request must have both
+    if (!username || !password) {
+      next({
+        name: "MissingCredentialsError",
+        message: "Please supply both a username and password"
+      });
+    }
+
+    const user = await getUserByUsername(username);
+    if( user ) {
+    try {
+        
+        const hashedPassword = user.password;
+        const isValid = await bcrypt.compare(password, hashedPassword);
+
+        if (user && isValid) {
+          // create token & return to user
+          const token = jwt.sign({ id: user.id, username: user.username }, process.env.JWT_SECRET);
+          req.user = user;
+          res.send({ success: true, user: user, message: "you're logged in!", token: token });
+        } else {
+          next({ 
+            name: 'IncorrectCredentialsError', 
+            message: 'Username or password is incorrect'
+          });
+        }
+      } catch(error) {
+        console.log(error);
+        next(error);
+      }
+    } else {
+      next({ 
+        name: 'NoSuchUserError', 
+        message: 'There is no user by that name. Please register'
+      });
+    }
+    });  
+
+    // POST /api/users/register
+router.post('/register', async (req, res, next) => {
+
+    const { username, password, emailAddress } = req.body;
+  
+    try {
+      const _user = await getUserByUsername(username);
+      
+      if(_user) {
+        next({
+          name: 'UserExistsError',
+          message: 'A user by that username already exists'
+        });
+      }
+      console.log('email', emailAddress);
+      const user = await createUser({username, password, emailAddress});
+      console.log('user', user);
+      const token = jwt.sign({id: user.id, username}, process.env.JWT_SECRET, {expiresIn: '1w'});
+      res.send({success: true, user: user, message: "Thank you for signing up", token});
+    } catch ({ name, message }) {
+        console.log('error', message);
+      next({ name, message})
+    }
+  });
+  
+  module.exports = router;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 import { useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Home } from './components';
+import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from "react-router-dom";
+import Register from "./RegisterPage"
+import Login from "./LoginPage"
 
 const App = () => {
     return (
-        <Home />
+        <div>
+
+<div className="topnav">
+  <a  href="/home">Home</a>
+  <a href="/register">Register</a>
+  <a href="/login">Login</a>
+</div>
+<div>
+<Routes>
+         
+         <Route path="/register" element={<Register/>}></Route>
+         <Route path="/login" element={<Login/>}></Route>
+         <Route path="/home" element={<Home/>}></Route>
+
+
+
+
+        
+</Routes>
+
+</div>
+        </div>
     )
 };
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<Router><App /></Router>);
