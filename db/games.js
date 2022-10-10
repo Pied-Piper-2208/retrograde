8b0c954a63fa0f6e17c36591b2ac64033ddc21a9
Jacen Gray <jjgray2003@gmail.com>
2022-10-10 09:54:39 -0500

    get game by id db

--- db/games.js
+++ db/games.js
@@ -30,4 +30,18 @@ const getAllGames = async () => {
     }
 };
 
+const getGameById = async (id) => {
+    try {
+        const { rows: [game] } = await client.query(`
+            SELECT *
+            FROM games
+            WHERE id = $1;
+        `,[id])
+        return game
+    } catch (error) {
+        console.error('Error getting game!')
+        throw error;
+    }
+}
+
 module.exports = { createGame, getAllGames };
