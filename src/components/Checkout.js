ad5079354740224091a82d3579357454496c8b7f
tbrown034 <tbrown034@gmail.com>
2022-10-12 16:09:03 -0500

    basic page

---
+++ src/components/Checkout.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Checkout = ({ cart, setCart }) => {
+    const handleDelete = (gameid) => {
+        setCart(cart.filter(({id})=>id!==gameid))
+    }
+    return (
+        <>
+        <div>
+            <h1>Checkout</h1>
+            <h2>Order Summary</h2>
+            <ul>
+                <li>Items: </li>
+                <li>Shipping and handling: $0.00</li>
+                <li>Total before taxes: </li>
+                <li>Estimated taxes: </li>
+                <li>Order total: </li>
+            </ul>
+
+
+
+        </div>
+        
+        
+
+        </>
+    )
+};
+
+export default Checkout
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
-import { Home, Details, Cart, AdminPage } from './components';
+import { Home, Details, Cart, AdminPage, Checkout } from './components';
 import Register from "./RegisterPage"
 import Login from "./LoginPage"
 
@@ -14,6 +14,7 @@ const App = () => {
             <Link to="/">Home</Link>
             <Link to="/admin">Admin Page</Link>
             <Link to="/cart">My Cart</Link>
+            <Link to="/checkout">Checkout</Link>
             <Link to="/register">Register</Link>
             <Link to="/login">Login</Link>
           </div>
@@ -25,6 +26,7 @@ const App = () => {
               <Route path="/login" element={<Login/>}></Route>
               <Route path="/:id" element={<Details cart={cart} setCart={setCart}/>}></Route>
               <Route path="/cart" element={<Cart cart={cart} setCart={setCart}/>}></Route>
+              <Route path="/checkout" element={<Checkout cart={cart} setCart={setCart}/>}></Route>
             </Routes>
           </div>
         </div>
